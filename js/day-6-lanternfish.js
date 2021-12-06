b7b980023e049bcc57022145c834bea655fe0691
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2021-12-05 22:15:17 -0800

    added solutions for day 6

---
+++ js/day-6-lanternfish.js
@@ -0,0 +1,28 @@
+"use strict";
+let fileSystem = require('fs');
+let inputFile = fileSystem.readFileSync("../temp-input.txt", "utf-8");
+const fishStates = inputFile.split(",");
+// ---------- Part 1 ----------
+(function () {
+    // convert values from strings to numbers
+    fishStates.forEach(function (element, index) {
+        fishStates[index] = Number(element);
+    })
+
+    // advances the countdown timer for every fish and adds new fish whenever 0 is reached
+    function incrementDay() {
+        for (let i = 0; i < fishStates.length; i++) {
+            fishStates[i]--;
+            if (fishStates[i] === -1) {
+                fishStates[i] = 6;
+                fishStates.push(9);
+            }
+        }
+    }
+
+    // run the function 80 times for 80 days
+    for (let i = 0; i < 80; i++) {
+        incrementDay();
+    }
+    console.log(fishStates.length);
+})();
