155d2b21957be8511840be02f0bf9d741320afb8
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2021-12-06 15:31:00 -0800

    fixed functions to not use same array

--- js/day-6-lanternfish.js
+++ js/day-6-lanternfish.js
@@ -5,17 +5,18 @@ let fishStates = inputFile.split(",");
 // ---------- Part 1 ----------
 (function () {
     // convert values from strings to numbers
+    let fishTracker = []
     fishStates.forEach(function (element, index) {
-        fishStates[index] = Number(element);
+        fishTracker.push(Number(element));
     })
 
     // advances the countdown timer for every fish and adds new fish whenever 0 is reached
     function incrementDay() {
-        for (let i = 0; i < fishStates.length; i++) {
-            fishStates[i]--;
-            if (fishStates[i] === -1) {
-                fishStates[i] = 6;
-                fishStates.push(9);
+        for (let i = 0; i < fishTracker.length; i++) {
+            fishTracker[i]--;
+            if (fishTracker[i] === -1) {
+                fishTracker[i] = 6;
+                fishTracker.push(9);
             }
         }
     }
@@ -24,29 +25,30 @@ let fishStates = inputFile.split(",");
     for (let i = 0; i < 80; i++) {
         incrementDay();
     }
-    console.log(fishStates.length);
+    console.log(fishTracker.length);
 })();
 // ---------- Part 2 (heavily inspired by https://github.com/LucasHMS) ----------
 (function () {
     // convert values from strings to numbers
+    let fishTracker = []
     fishStates.forEach(function (element, index) {
-        fishStates[index] = Number(element);
+        fishTracker.push(Number(element));
     })
     // put all the fish into an array with their index based on the number of days until spawning a new fish
     let fishCountPerDay = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     let babyFish = 0;
     let totalFish = 0;
-    fishStates.forEach(function (element) {
+    fishTracker.forEach(function (element) {
         fishCountPerDay[element]++;
     });
     // for each day, move all fish at count 0 to count 7 and shift array, then push the original count 0 fish to begin at index 8
-    for(let i = 0; i < 256; i++) {
+    for (let i = 0; i < 256; i++) {
         fishCountPerDay[7] += fishCountPerDay[0];
         babyFish = fishCountPerDay.shift();
         fishCountPerDay.push(babyFish);
     }
     // add the amounts of fish in each count together for the solution
-    fishCountPerDay.forEach(function(element) {
+    fishCountPerDay.forEach(function (element) {
         totalFish += element;
     })
     console.log(totalFish);
